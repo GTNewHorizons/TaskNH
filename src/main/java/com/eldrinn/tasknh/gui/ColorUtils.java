eef9672f7c7d8e669f4b89f8907c171ecdcd2d1a
Ranzu <66495944+Ranzuu@users.noreply.github.com>
2026-09-05 17:02:40 +0200

    class cleaning

--- src/main/java/com/eldrinn/tasknh/gui/ColorUtils.java
+++ src/main/java/com/eldrinn/tasknh/gui/ColorUtils.java
@@ -2,45 +2,40 @@ package com.eldrinn.tasknh.gui;
 
 import com.gtnewhorizon.gtnhlib.color.ColorResource;
 
-public final class ColorUtils {
+
+public class ColorUtils {
 
     private static final ColorResource.Factory color = new ColorResource.Factory("tasknh");
 
-    // Text
-    public static final ColorResource TEXT_WHITE = color.rgb("textWhite", "0xFFFFFF");
-    public static final ColorResource TEXT_GRAY = color.rgb("textGray", "0xAAAAAA");
+    public static final ColorResource
+    // spotless:off
+        textWhite               = color.rgb("textWhite",                "0xFFFFFF"),
+        textGray                = color.rgb("textGray",                 "0xAAAAAA"),
 
-    // Accents
-    public static final ColorResource GOLD = color.rgb("gold", "0xF0C040");
-    public static final ColorResource GREEN = color.rgb("green", "0x8BC34A");
+        accentGold              = color.rgb("accentGold",               "0xF0C040"),
+        accentGreen             = color.rgb("accentGreen",              "0x8BC34A"),
 
-    // Icons
-    public static final ColorResource ICON_ADD = color.argb("iconAdd", "0xFF40C040");
-    public static final ColorResource ICON_REMOVE = color.argb("iconRemove", "0xFFC04040");
-    public static final ColorResource PIN_ACTIVE = color.argb("pinActive", "0xFFF0C040");
-    public static final ColorResource PIN_INACTIVE = color.argb("pinInactive", "0xFF555555");
+        mapWaypointOpen         = color.rgb("mapWaypointOpen",          "0xFFAAAA"),
+        mapWaypointInProgress   = color.rgb("mapWaypointInProgress",    "0xAAAAFF"),
+        mapWaypointDone         = color.rgb("mapWaypointDone",          "0xAAFFAA"),
 
-    // Backgrounds
-    public static final ColorResource BG_HUD = color.argb("bgHud", "0x88000000");
-    public static final ColorResource BG_OVERLAY = color.argb("bgOverlay", "0x44000000");
-    public static final ColorResource BG_PANEL = color.argb("bgPanel", "0xCC000000");
-    public static final ColorResource BG_BUTTON = color.argb("bgButton", "0xFF444444");
-    public static final ColorResource BG_DANGER = color.argb("bgDanger", "0xFF884444");
-    public static final ColorResource HANDLE = color.argb("handle", "0xFFCC3333");
+        mapFillOpen             = color.rgb("mapFillOpen",              "0x888800"),
+        mapFillInProgress       = color.rgb("mapFillInProgress",        "0x004488"),
+        mapFillDone             = color.rgb("mapFillDone",              "0x228822"),
 
-    // Map waypoints (light tones, Navigator API)
-    public static final ColorResource MAP_WAYPOINT_OPEN = color.rgb("mapWaypointOpen", "0xFFAAAA");
-    public static final ColorResource MAP_WAYPOINT_IN_PROGRESS = color.rgb("mapWaypointInProgress", "0xAAAAFF");
-    public static final ColorResource MAP_WAYPOINT_DONE = color.rgb("mapWaypointDone", "0xAAFFAA");
+        mapText                 = color.rgb("mapText",                  "0xFFFFFF"),
+        mapBorder               = color.rgb("mapBorder",                "0xFFFFFF"),
 
-    // Map markers fill (darker tones, separate alpha in DrawUtils)
-    public static final ColorResource MAP_FILL_OPEN = color.rgb("mapFillOpen", "0x888800");
-    public static final ColorResource MAP_FILL_IN_PROGRESS = color.rgb("mapFillInProgress", "0x004488");
-    public static final ColorResource MAP_FILL_DONE = color.rgb("mapFillDone", "0x228822");
+        iconAdd                 = color.argb("iconAdd",                 "0xFF40C040"),
+        iconRemove              = color.argb("iconRemove",              "0xFFC04040"),
+        iconPinActive           = color.argb("iconPinActive",           "0xFFF0C040"),
+        iconPinInactive         = color.argb("iconPinInactive",         "0xFF555555"),
 
-    // Map label
-    public static final ColorResource MAP_TEXT = color.argb("mapText", "0xFFFFFFFF");
-    public static final ColorResource MAP_BORDER = color.rgb("mapBorder", "0xFFFFFF");
-
-    private ColorUtils() {}
+        backgroundHud           = color.argb("backgroundHud",           "0x88000000"),
+        backgroundOverlay       = color.argb("backgroundOverlay",       "0x44000000"),
+        backgroundPanel         = color.argb("backgroundPanel",         "0xCC000000"),
+        backgroundButton        = color.argb("backgroundButton",        "0xFF444444"),
+        backgroundDanger        = color.argb("backgroundDanger",        "0xFF884444"),
+        backgroundHandle        = color.argb("backgroundHandle",        "0xFFCC3333");
+    // spotless:on
 }
